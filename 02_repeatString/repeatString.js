26dd617f36fb41998035e3d144cfd98982f824d1
Justin Donner <justinmdonner@gmail.com>
2023-03-20 15:18:07 -0400

    Finish tasks

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+   if(num < 0) return "ERROR";
+    
+   string = string.repeat(num);
+    return string;
 };
 
 // Do not edit below this line
