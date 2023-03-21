1e68f000555a671df60333a3543aaf5946fd1c39
Justin Donner <justinmdonner@gmail.com>
2023-03-21 10:35:03 -0400

    Add alternative approach

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,9 +1,20 @@
-const repeatString = function(string, num) {
+const repeatString = function(word, num) {
 
-   if(num < 0) return "ERROR";
+    let string = '';
     
-   string = string.repeat(num);
+    if(num < 0) return "ERROR";
+
+    for(i=0;i<num;i++){  
+
+        string += word;    
+    }
+
     return string;
+ 
+
+    
+//    string = string.repeat(num);
+//     return string;
 };
 
 // Do not edit below this line
